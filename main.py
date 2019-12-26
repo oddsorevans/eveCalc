7607f7951df577ef41923ef1b447e3cb13a514ce
Evan Jackson <evan@Evans-Air.attlocal.net>
2019-12-25 21:28:19 -0600

    checks year

--- main.py
+++ main.py
@@ -3,10 +3,27 @@
 from datetime import date
 
 def dateDistance(date, holiday):
-    return abs(date - holiday).days
+    distance = abs(date - holiday).days
+    print(distance)
+    return distance
 
-d1 = date(2019,12,30)
-christmas = date(2019,12,25)
+def findYear(date, holiday):
+    if date.month < holiday.month:
+        holiday = holiday.replace(date.year)
+    elif date.month > holiday.month:
+        holiday = holiday.replace(date.year + 1)
+    else:
+        if date.day > holiday.day:
+            holiday = holiday.replace(date.year + 1)
+        else:
+            holiday = holiday.replace(date.year) 
+    return holiday
+
+
+d1 = date(2019,12,24)
+christmas = date(1,12,25)
+
+christmas = findYear(d1, christmas)
 
 eve = "Merry Christmas"
 
