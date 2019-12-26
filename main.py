85a5e8be3add2ddc4565ea223c426d937172e5ed
Evan Jackson <evan@Evans-MacBook-Air.local>
2019-12-26 17:58:34 -0600

    added comments

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 
 from datetime import date
 
+#finds the distance between 2 dates. Prints distance for testing
 def dateDistance(date, holiday):
     distance = abs(date - holiday).days
     print(distance)
     return distance
 
+#The holiday has a dummy year, since that could change depending on the current date.
+#To get around that, the program finds the year of the next time the holiday will occur
+#dependent on the date given
 def findYear(date, holiday):
     if date.month < holiday.month:
         holiday = holiday.replace(date.year)
@@ -21,12 +25,15 @@ def findYear(date, holiday):
 
 
 d1 = date(2019,12,24)
+#1 is a dummy year. Will be used to check if it is a user created year
 christmas = date(1,12,25)
 
 christmas = findYear(d1, christmas)
 
 eve = "Merry Christmas"
 
+#print out eve for distance. If date distance is 0, it is that day
+#and never concatenates eve
 for i in range(0, dateDistance(d1, christmas)):
     eve = eve + " eve"
 
